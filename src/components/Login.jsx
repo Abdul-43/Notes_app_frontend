3309fbc1562858041da1f45cfbaf425dc28b399b
Abdul Rahman <64832120+Abdul-43@users.noreply.github.com>
2023-12-23 02:54:08 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,7 @@ export const Login = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`http://localhost:8000/api/auth/login`, {
+    const response = await fetch(`https://notes-app-backend-vlll.onrender.com/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
